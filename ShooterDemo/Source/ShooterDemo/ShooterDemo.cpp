3cbad3c4991482e367642236e746773133125bee
kevintheiss <ktheiss1990@gmail.com>
2023-11-28 11:06:33 -0700

    First commit

--- ShooterDemo/Source/ShooterDemo/ShooterDemo.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "ShooterDemo.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ShooterDemo, "ShooterDemo" );
- 
